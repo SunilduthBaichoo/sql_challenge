5a3808c1e136795f8c53f1e7b1ae9b02b4c80cfd
SunilduthBaichoo <sunilduth.baichoo@gmail.com>
2024-11-09 14:35:15 -0500

    updated ERD,Schema and Data_Analysis files

--- data_analysis_updated.sql
+++ data_analysis_updated.sql
@@ -65,13 +65,14 @@ WHERE d.dept_name = 'Sales';
 
 -- 7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
-SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
-FROM dept_emp as de
-INNER JOIN departments as d
-ON de.dept_no = d.dept_no
-INNER JOIN employees as e
-ON de.emp_no = e.emp_no
-WHERE (d.dept_name = 'Sales') OR (d.dept_name = 'Development') ;
+SELECT e.emp_no,
+       e.last_name,
+       e.first_name,
+       d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
 
 
 -- 8 List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
